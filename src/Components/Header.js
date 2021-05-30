c00235ebe4a6db57c2aeecdf92d76e97fd6c8b7d
Brock Mortenson <mortensonbrock@gmail.com>
2021-05-30 01:32:14 -0600

    I had an epiphany after I finished up earlier today on how to do the pop up navbar for mobile view so I got that figured out.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,41 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/header.css';
 
 function Header() {
+    const [ open, setOpen ] = useState(false);
+
+    function handleOpen() {
+        setOpen(!open);
+    }
+
     return (
         <div className='Header'>
             <div>
                 <h1>BROCK MORTENSON</h1>
                 <div>
-                    <input className='toggler' type='checkbox' />
+                    <input className='toggler' type='checkbox' onClick={handleOpen} />
                     <div className='hamburger'><div></div></div>
                 </div>
             </div>
             <nav>
-                <div>
+                <div className='navbar'>
                     <Link to='/' style={{ textDecoration: 'none', color: 'black' }}><p>HOME</p></Link>
                     <Link to='/Skills' style={{ textDecoration: 'none', color: 'black' }}><p>SKILLS</p></Link>
                     <Link to='/Experience' style={{ textDecoration: 'none', color: 'black' }}><p>EXPERIENCE</p></Link>
                     <Link to='/Projects' style={{ textDecoration: 'none', color: 'black' }}><p>PROJECTS</p></Link>
                     <Link to='/Resume' style={{ textDecoration: 'none', color: 'black' }}><p>RESUME</p></Link>
                 </div>
+                { open ?
+                    <div className='toggled'>
+                        <Link to='/' style={{ textDecoration: 'none', color: 'black' }}><p>HOME</p></Link>
+                        <Link to='/Skills' style={{ textDecoration: 'none', color: 'black' }}><p>SKILLS</p></Link>
+                        <Link to='/Experience' style={{ textDecoration: 'none', color: 'black' }}><p>EXPERIENCE</p></Link>
+                        <Link to='/Projects' style={{ textDecoration: 'none', color: 'black' }}><p>PROJECTS</p></Link>
+                        <Link to='/Resume' style={{ textDecoration: 'none', color: 'black' }}><p>RESUME</p></Link>
+                    </div>
+                    : null
+                    }
             </nav>
         </div>
     )
