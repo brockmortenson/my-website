c29a5756d2b63045bb546e803b8fada9d23693cb
Brock Mortenson <mortensonbrock@gmail.com>
2021-05-31 15:39:59 -0600

    Fixes for the about me section

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -18,9 +18,9 @@ function Landing() {
                     </section>
                     <div className='biography'>
                         <h1>About Me</h1>
-                        <p>I am a full-stack developer in Logan, Utah with a passion for both the hardware and software aspects of computers. In 2021, I attended an immersive coding bootcamp named Devmountain, where I trained in React-based applications, learning full-stack web development. The full-stack I was taught included React, Node, Express, and PostgreSQL.</p>
+                        <p>I am a full-stack developer in Logan, Utah with a passion for both the hardware and software aspects of computers. I am a graduate of Devmountain's Web Development Program, where I trained in React-based applications, learning full-stack web development. The full-stack that I am proficient in includes React, Node, Express, and PostgreSQL.</p>
                         <br />
-                        <p>In my free time I occasionally run marathons, play video games, and build computers. I also enjoy casually coding and developing simple web applications such as this website. </p>
+                        <p>In my free time I occasionally run marathons, play video games, and build computers. I also enjoy casually coding, learning new technologies along with their respective syntax and developing web applications such as this website. </p>
                     </div>
                 </div>
             </div>
